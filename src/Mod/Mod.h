f1722464536cb3489c0fc2db67cca3a16cd083bd
Sopukl <kwaga65536@gmail.com>
2026-08-12 17:01:30 +0300

    -

--- src/Mod/Mod.h
+++ src/Mod/Mod.h
@@ -18,12 +18,11 @@
  * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-//#include "../Engine/Yaml.h"
 #include "../Engine/Options1.h"
 #include "../Engine/Collections.h"
 #include "../Savegame/GameTime.h"
 #include "../Savegame/Soldier.h"
-#include "RuleDamageType.h"
+// #include "RuleDamageType.h"
 #include "RuleAlienMission.h"
 #include "RuleBaseFacilityFunctions.h"
 #include "RuleItem.h"
