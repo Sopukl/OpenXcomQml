534b923ada107a777fc15308ddee3fbf961f4b1c
Sopukl <65536@inbox.ru>
2026-09-15 13:29:28 +0300

    Menu->Qml

--- src/Menu/OptionsBaseState.h
+++ src/Menu/OptionsBaseState.h
@@ -17,62 +17,15 @@
  * You should have received a copy of the GNU General Public License
  * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "../Engine/State.h"
 
 
 namespace OpenXcom
 {
-
-enum OptionsOrigin
-{
-	OPT_MENU,
-	OPT_GEOSCAPE,
-	OPT_BATTLESCAPE
-};
-
-class Window;
-class TextButton;
-class Text;
-
-/**
- * Options base state for common stuff
- * across Options windows.
- */
-class OptionsBaseState : public State
-{
-protected:
-	OptionsOrigin _origin;
-	Window *_window;
-	TextButton *_btnVideo, *_btnAudio, *_btnControls, *_btnGeoscape, *_btnBattlescape, *_btnAdvanced, *_btnFolders;
-	TextButton *_btnOk, *_btnCancel, *_btnDefault;
-	Text *_txtTooltip;
-	std::string _currentTooltip;
-	TextButton *_group;
-public:
-	/// Creates the Options state.
-	OptionsBaseState(OptionsOrigin origin);
-	/// Cleans up the Options state.
-	~OptionsBaseState();
-	/// Restarts the game states.
-	static void restart(OptionsOrigin origin);
-	/// Initializes palettes.
-	void init() override;
-	/// Presses a certain category button.
-	void setCategory(TextButton *button);
-	/// Handler for clicking the OK button.
-	void btnOkClick(Action *action);
-	/// Handler for clicking the Cancel button.
-	void btnCancelClick(Action *action);
-	/// Handler for clicking the Restore Defaults button.
-	void btnDefaultClick(Action *action);
-	/// Handler for clicking one of the grouping buttons.
-	void btnGroupPress(Action *action);
-	/// Handler for showing tooltip.
-	void txtTooltipIn(Action *action);
-	/// Handler for hiding tooltip.
-	void txtTooltipOut(Action *action);
-	/// Update the resolution settings, we just resized the window.
-	void resize(int &dX, int &dY) override;
-};
+	enum OptionsOrigin
+	{
+		OPT_MENU,
+		OPT_GEOSCAPE,
+		OPT_BATTLESCAPE
+	};
 
 }
